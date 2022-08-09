18d741f4bad559b750f8fde1c58c03a42d1abd85
akshay <kumarakshay6979@gmail.com>
2022-08-09 11:38:03 +0530

    loged again

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Square = ({ value }) => {
+  return (
+    <button type="button" className="square" onClick={()={
+        console.log(value)
+    }}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
